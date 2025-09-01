7ad2ce73e87ee54775416d9ca3c8e219003ea109
kycv7 <kycv7@outlook.com>
2025-09-01 16:35:15 -0600

    carga de datos

---
+++ modelo.py
@@ -0,0 +1,16 @@
+#cargar datos
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import kagglehub
+path=kagglehub.dataset_download("yasserh/titanic-dataset")
+print("path to dataset files: ", path)
+import os
+path = r"C:\Users\cklyf\.cache\kagglehub\datasets\yasserh\titanic-dataset\versions\1"
+print(os.listdir(path))
+df=pd.read_csv(path + "/Titanic-Dataset.csv")
+print(df.head())
